8120c8282c33f7f7fa7081e67fcc5f48b05b6206
Aryan Singh <ary1733@gmail.com>
2022-01-18 11:20:29 +0000

    temp commit

--- Assn1/Q1e/Assgn1_1e_54_19CS10069_19CS30007.sh
+++ Assn1/Q1e/Assgn1_1e_54_19CS10069_19CS30007.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+# REQ_HEADERS="\"User-Agent\",\"Host\"";
+REQ_HEADERS="User-Agent,Host";
+RESPONSE=$(curl -s  "http://headers.jsontest.com/")
+# response=$(curl "http://headers.jsontest.com/")
+echo $RESPONSE | jq .Host
+echo $REQ_HEADERS | tr "," "\n" | awk '{system("echo $RESPONSE | jq ."$0" ")}'
+# response=$(curl "http://headers.jsontest.com/")
+# echo $response;
+# REQ_SPLIT=(${REQ_HEADERS//,/ })
+# echo ${REQ_SPLIT[@]}
+# curl -s  "http://headers.jsontest.com/" | jq ".[\"${REQ_SPLIT[@]}\"]"
+# echo ${REQ_HEADERS[1]
+# curl -s  "http://headers.jsontest.com/" | jq ".[$REQ_HEADERS]"
+# echo ${REQ_SPLIT[1]};
+# curl -s  "http://headers.jsontest.com/" | jq ".[\"Host\",\"User-Agent\"]"
+# curl -v -H "X-Custom-Header: Aryan" "http://headers.jsontest.com/";
+# grep User-Agent < response
+# curl -v -H "X-Custom-Header: Aryan" "http://headers.jsontest.com/" ;
+# curl -I -H "X-Custom-Header: Aryan" "http://headers.jsontest.com/" ;
+
+# 2>&1 | echo;
+# echo $response | python3 -c "import sys, json; print(sys.stdin))"
+# curl http://headers.jsontest.com/ > example2.json
