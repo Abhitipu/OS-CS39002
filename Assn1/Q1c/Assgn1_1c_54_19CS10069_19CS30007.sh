b4521551b11f3535d0ac48ed66e9d7a96a1d58f7
Aryan Singh <ary1733@gmail.com>
2022-01-20 04:50:44 +0000

    c done, white space reduction left

--- Assn1/Q1c/Assgn1_1c_54_19CS10069_19CS30007.sh
+++ Assn1/Q1c/Assgn1_1c_54_19CS10069_19CS30007.sh
@@ -1,2 +1,4 @@
-find $1 -type f | awk -F '/' '{n=split($NF, parts, "."); ext=(parts[n]==$NF)?"nil":parts[n]; system("mkdir -p " ext); system("cp " $0 " " ext);}';
-rm -r $1;
+mkdir -p nil
+find $1 -mindepth 1 -type f -exec mv -t ./nil -f '{}' +
+rm -rf $1
+find nil -type f | awk -F "/" '{n=split($NF, a, "."); if(a[n] != $NF) ext[a[n]]++;} END{ for (e in ext) {system("mkdir -p "e";echo ./nil/*."e" | xargs mv -t ./"e)}}'
