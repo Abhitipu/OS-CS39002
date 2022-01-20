aac22ed027775301bbf550745836eddbb34cd750
Aryan Singh <ary1733@gmail.com>
2022-01-20 16:09:24 +0000

    6-7 minimized

--- Assn1/Q1f/Assgn1_1f_54_19CS10069_19CS30007.sh
+++ Assn1/Q1f/Assgn1_1f_54_19CS10069_19CS30007.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{count[tolower($variable)]++} END {for (i in count) print i, count[i]}' variable=$2 $1 | sort -k2 -rn > "1f_output_$2_column.freq"
+awk '{count[tolower($variable)]++}END{for(i in count)print i,count[i]}' variable=$2 $1|sort -k2 -rn>"1f_output_$2_column.freq"
--- Assn1/Q1g/Assgn1_1g_54_19CS10069_19CS30007.sh
+++ Assn1/Q1g/Assgn1_1g_54_19CS10069_19CS30007.sh
@@ -1,9 +1,9 @@
-echo -n >$1
-for i in {1..150}; do
-    for j in {1..10}; do
-        echo -n "$RANDOM," >> $1;
+echo -n>$1
+for i in {1..150};do
+    for j in {1..10};do
+        echo -n "$RANDOM,">>$1;
     done
-    echo "" >> $1;
+    echo "">>$1;
 done
 
-awk -F ',' '{ if ( $colnum ~ regex ){ ok="YES" } } END{print ok}' ok="NO" colnum=$2 regex=$3 $1
+awk -F ',' '{if($colnum~regex){ok="YES"}}END{print ok}' ok="NO" colnum=$2 regex=$3 $1
