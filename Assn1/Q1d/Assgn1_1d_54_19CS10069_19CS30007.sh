40f376502ef02b987529aa0c12f45ac382305d25
Abhitipu <abhinandan0316@gmail.com>
2022-01-21 13:07:04 +0530

    Minor updates | v1 done

--- Assn1/Q1d/Assgn1_1d_54_19CS10069_19CS30007.sh
+++ Assn1/Q1d/Assgn1_1d_54_19CS10069_19CS30007.sh
@@ -1,4 +1,4 @@
 mkdir -p files_mod
 for filename in $1/*;do
-    awk '{print NR" "$s}' $filename|tr -s ' ' ',' >"files_mod/$(basename $filename)"
+    awk '{print NR" "$s}' $filename|tr -s ' ' ','>"files_mod/$(basename $filename)"
 done
--- Assn1/Q1e/Assgn1_1e_54_19CS10069_19CS30007.sh
+++ Assn1/Q1e/Assgn1_1e_54_19CS10069_19CS30007.sh
@@ -1,11 +1,30 @@
-REQ_HEADERS="User-Agent,Host";
-curl -s "https://www.example.com/">example.html
-curl "http://ip.jsontest.com/"
-echo $REQ_HEADERS | tr "," "\n" | awk '{system("curl -s http://headers.jsontest.com/ | grep "$0)}'
+_V=0
 
+while getopts "v" OPTION
+do
+  case $OPTION in
+    v) _V=1
+       ;;
+  esac
+done
+
+[[ $_V -eq 1 ]] && echo "Fetching html from example.com"
+curl -s "https://www.example.com/">example.html
+[[ $_V -eq 1 ]] && echo "Completed task 1"
+
+[[ $_V -eq 1 ]] && echo "Obtaining ip address of the local system"
+curl "http://ip.jsontest.com/"
+[[ $_V -eq 1 ]] && echo "Completed task 2"
+
+[[ $_V -eq 1 ]] && echo "Obtaining headers from headers.jsontest.com"
+echo $REQ_HEADERS|tr "," "\n"|awk '{system("curl -s http://headers.jsontest.com/|grep "$0)}'
+[[ $_V -eq 1 ]] && echo "Completed task 3"
+
+[[ $_V -eq 1 ]] && echo "Checking validity of json files"
 >valid.txt
 >invalid.txt
-for file in ./JSONData/*; do 
-    verdict=$(curl -s --data-urlencode json@$file http://validate.jsontest.com  | jq .validate)
+for file in ./JSONData/*;do 
+    verdict=$(curl -s --data-urlencode json@$file http://validate.jsontest.com|jq .validate)
     [[ "$verdict" = "true" ]] && echo $file>>valid.txt || echo $file>>invalid.txt;
 done
+[[ $_V -eq 1 ]] && echo "Completed task 4.. All done!"
