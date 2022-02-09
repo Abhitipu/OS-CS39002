02121012bd76e90fb663a876f56ea89499202169
Abhitipu <abhinandan0316@gmail.com>
2022-02-09 09:14:10 +0530

    Reorganized and added makefiles

--- Assn3/Part1/Ass3_Part1_54_19CS10069_19CS30007.cpp
+++ Assn3/Part1/Ass3_Part1_54_19CS10069_19CS30007.cpp
@@ -169,35 +169,3 @@ int main(int argc, char *argv[]) {
     shm_unlink(matrix3);
     return 0;
 }
-
-/*
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-int main()
-{
-//     /* the size (in bytes) of shared memory object */
-//     const int SIZE = 4096;
-//     /* create the shared memory object */
-//     shm fd = shm open(name, O CREAT | O RDRW, 0666);
-//     /* strings written to shared memory */
-//     const char *message 0 = "Hello";
-//     const char *message 1 = "World!";
-//     /* shared memory file descriptor */
-//     int shm fd;
-//     /* pointer to shared memory obect */
-//     void *ptr;
-    
-//     /* configure the size of the shared memory object */
-//     ftruncate(shm fd, SIZE);
-//     /* memory map the shared memory object */
-//     ptr = mmap(0, SIZE, PROT WRITE, MAP SHARED, shm fd, 0);
-//     /* write to the shared memory object */
-//     sprintf(ptr,"%s",message 0);
-//     ptr += strlen(message 0);
-//     sprintf(ptr,"%s",message 1);
-//     ptr += strlen(message 1);
-//     return 0;
-// }
